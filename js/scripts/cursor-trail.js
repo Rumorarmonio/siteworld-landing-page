2363254c03cb783714d65a74525df786739674b8
Egor Skleynov <yxllwllxy@gmail.com>
2023-10-20 11:11:00 +0800

    add sliders to stories, change some changes

--- js/scripts/cursor-trail.js
+++ js/scripts/cursor-trail.js
@@ -1,28 +1,28 @@
-const canvas = document.querySelector('.intro-animation');
+const canvas = document.querySelector('.intro-animation')
 
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.style.filter = "blur(70px)";
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+canvas.style.filter = 'blur(70px)'
 
-const particles = [];
-const iterations = 1;
-const initialParticleSize = 100;
+const particles = []
+const iterations = 1
+const initialParticleSize = 100
 
-let currentHue = 150;
-const minHue = 150;
-const maxHue = 310;
+let currentHue = 150
+const minHue = 150
+const maxHue = 310
 let hueIncrement = true
 
-let scroll = 0;
+let scroll = 0
 
 window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 })
 
 window.addEventListener('scroll', () => {
-    scroll = document.documentElement.scrollTop;
+    scroll = document.documentElement.scrollTop
 })
 
 const mouse = {
@@ -32,84 +32,84 @@ const mouse = {
 
 // if call addEventListener on a canvas element,
 // it will not detect mouse movement on top of other elements
-const container = document.querySelector('.page__container_upper');
+const container = document.querySelector('.page__container_upper')
 container.addEventListener('mousemove', event => {
-    mouse.x = event.x;
-    mouse.y = event.y;
+    mouse.x = event.x
+    mouse.y = event.y
     for (let i = 0; i < iterations; i++) {
-        particles.push(new Particle());
+        particles.push(new Particle())
     }
 })
 
 class Particle {
     constructor() {
-        this.x = mouse.x;
-        this.y = mouse.y + scroll;
+        this.x = mouse.x
+        this.y = mouse.y + scroll
 
         // uncomment to generate particles randomly everywhere
         // this.x = Math.random() * canvas.width;
         // this.y = Math.random() * canvas.height;
 
-        this.size = initialParticleSize;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
-        this.color = `hsl(${currentHue}, 50%, 50%)`;
+        this.size = initialParticleSize
+        this.speedX = Math.random() * 3 - 1.5
+        this.speedY = Math.random() * 3 - 1.5
+        this.color = `hsl(${currentHue}, 50%, 50%)`
     }
 
     update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
+        this.x += this.speedX
+        this.y += this.speedY
         if (this.size > .1) {
-            this.size -= 1;
+            this.size -= 1
         }
     }
 
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
+        ctx.fillStyle = this.color
+        ctx.beginPath()
         // start at center point (0, 0) of 100px 100px, start at 0 (far right), end at a full circle
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx.fill()
     }
 }
 
 function handleParticles() {
     for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
-        particles[i].draw();
+        particles[i].update()
+        particles[i].draw()
         if (particles[i].size <= .3) {
-            particles.splice(i, 1);
-            i--;
+            particles.splice(i, 1)
+            i--
         }
     }
 }
 
 function animate() {
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#001036';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#001036'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     // check if hue value is in range between min and max values
     if (hueIncrement) {
-        currentHue += 1;
+        currentHue += 1
 
         if (currentHue === maxHue) {
-            hueIncrement = false;
+            hueIncrement = false
         }
     } else {
-        currentHue -= 1;
+        currentHue -= 1
 
         if (currentHue === minHue) {
-            hueIncrement = true;
+            hueIncrement = true
         }
     }
 
     // uncomment to let particles take all RGB colors
     // currentHue += 1;
 
-    handleParticles();
+    handleParticles()
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate)
 
     // uncomment to lower fps
     // setTimeout(() => {
@@ -117,6 +117,6 @@ function animate() {
     // }, 1000 / 30);
 }
 
-animate();
+animate()
 
 // setTimeout(animate, 3000)
